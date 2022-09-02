1fc4cab0c08347e70040a4ef5bbfa989deb1f6bd
Roy Angelsen <roy.angelsen@elementlogic.no>
2022-09-02 15:15:42 +0200

    Render hello triangle

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,37 @@
+{-# LANGUAGE ScopedTypeVariables, PackageImports, TypeFamilies #-} 
+
 module Main (main) where
 
-import Lib
+import Graphics.GPipe
+import qualified "GPipe-GLFW4" Graphics.GPipe.Context.GLFW as GLFW
+import Control.Monad (unless)
 
 main :: IO ()
-main = someFunc
+main =
+  runContextT GLFW.defaultHandleConfig $ do
+    win <- newWindow (WindowFormatColor RGB8) (GLFW.defaultWindowConfig "Hello world!")
+
+    vertexBuffer :: Buffer os (B4 Float, B3 Float) <- newBuffer 3
+    writeBuffer vertexBuffer 0 [ (V4 (-1) 1 0 1, V3 1 0 0)
+                               , (V4 0 (-1) 0 1, V3 0 1 0)
+                               , (V4 1 1 0 1,  V3 0 0 1)
+                               ]
+
+    shader <- compileShader $ do
+      primitiveStream <- toPrimitiveStream id
+      fragmentStream <- rasterize (const (FrontAndBack, PolygonFill, ViewPort (V2 0 0) (V2 500 500), DepthRange 0 1)) primitiveStream
+      drawWindowColor (const (win, ContextColorOption NoBlending (V3 True True True))) fragmentStream
+
+    loop vertexBuffer shader win
+
+loop vertexBuffer shader win = do
+  render $ do
+    clearWindowColor win (V3 0 0 0)
+    vertexArray <- newVertexArray vertexBuffer
+    let primitiveArray = toPrimitiveArray TriangleList vertexArray
+    shader primitiveArray
+  swapWindowBuffers win
+
+  closeRequested <- GLFW.windowShouldClose win
+  unless (closeRequested == Just True) $
+    loop vertexBuffer shader win   
